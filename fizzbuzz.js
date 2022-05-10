4671d3301853854c421fe64c52199e297f13bfe9
KaisenFletcher <mbiggs2000@outlook.com>
2022-05-10 18:44:00 +0100

    1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,14 @@
-
+for (let i = 1; i < 101; i++) {
+    if( i % 3 == 0 && i % 5 == 0) {
+        output("FizzBuzz");
+    }else if( i % 5 == 0 ) {
+        output("Buzz")
+    }else if( i % 3 == 0 ) {
+        output("Fizz");
+    }
+    else {
+    output(i)
+    }
+}
 // More Information about this assisngment is available in the README.md file
 
